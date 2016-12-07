585f3a909a7fbb16dc10d0ba66dac226268b4d99
Alex <alex1000209@gmail.com>
2016-12-07 14:41:34 +0100

    Added example project
    Added an example project for the utilities scripts

---
+++ VR-utlities/exampleProject/Assets/GrabObjectScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabObjectScript : MonoBehaviour
+{
+	public LayerMask grabables;
+	public float radius = 0.07f;
+	SteamVR_TrackedObject trackedObj;
+	GameObject grabbedObject = null;
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+							   
+	void Start ()
+	{
+		SphereCollider col = gameObject.AddComponent<SphereCollider>();
+		col.isTrigger = true;
+		col.radius = radius;
+		col.center = new Vector3(0f, -0.03f, 0f);
+	}
+							  
+	void FixedUpdate ()
+	{
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+		if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger) && grabbedObject != null)
+		{
+			Collider col = grabbedObject.GetComponent<Collider>();
+			col.attachedRigidbody.isKinematic = false;
+			col.gameObject.transform.SetParent(null);
+			tossObject(col.attachedRigidbody, device);
+			grabbedObject = null;
+		} 
+		
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && grabbedObject == null)
+        {
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent(this.transform);
+			grabbedObject = col.gameObject;
+		} 
+	}
+
+	void tossObject(Rigidbody rigidbody, SteamVR_Controller.Device device)
+	{
+		Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+		if (origin != null)
+		{
+			rigidbody.velocity = origin.TransformVector(device.velocity);
+			rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+		}
+		else
+		{
+			rigidbody.velocity = device.velocity;
+			rigidbody.angularVelocity = device.angularVelocity;
+		}
+	}
+}
---
+++ VR-utlities/exampleProject/Assets/LaserTeleport.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public struct LaserEventArgs
+{
+	public uint controllerIndex;
+	public uint flags;
+	public float distance;
+	public Transform target;
+}
+
+public delegate void LaserEventHandler(object sender, LaserEventArgs e);
+
+public class LaserTeleport : MonoBehaviour {
+
+
+	public LayerMask walkables;
+	public float maxTelDst = 2f;
+	public bool active = true;
+	public Color color;
+	public float thickness = 0.002f;
+	GameObject holder;
+	GameObject pointer;
+	public Transform head;
+	bool isActive = false;
+	Transform reference;
+	public event LaserEventHandler PointerIn;
+	public event LaserEventHandler PointerOut;
+
+	Vector3 lastPoint;
+
+	Transform previousContact = null;
+
+	// Use this for initialization
+	void Start()
+	{
+		var trackedController = GetComponent<SteamVR_TrackedController>();
+		if (trackedController == null)
+		{
+			trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
+		}
+
+		trackedController.PadClicked += new ClickedEventHandler(DoClick);
+
+		holder = new GameObject();
+		holder.transform.parent = this.transform;
+		holder.transform.localPosition = Vector3.zero;
+		holder.transform.rotation = this.transform.rotation;
+
+
+		pointer = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Destroy((UnityEngine.Object)pointer.GetComponent<Collider>());
+		pointer.transform.parent = holder.transform;
+		pointer.transform.localScale = new Vector3(thickness, thickness, 100f);
+		pointer.transform.localPosition = new Vector3(0f, 0f, 50f);
+		Material newMaterial = new Material(Shader.Find("Unlit/Color"));
+		newMaterial.SetColor("_Color", color);
+		pointer.GetComponent<MeshRenderer>().material = newMaterial;
+	}
+
+	private void DoClick(object sender, ClickedEventArgs e)
+	{
+		//Teleport to laser point
+		Ray ray = new Ray(holder.transform.position, holder.transform.forward);	
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast(ray, out hit, maxTelDst, walkables))
+		{
+			lastPoint = hit.point;
+			Vector3 offset = new Vector3(transform.parent.position.x - head.position.x, 0, transform.parent.position.z - head.position.z);
+			transform.parent.position = hit.point + offset;
+		}
+	}
+
+	public virtual void OnPointerIn(LaserEventArgs e)
+	{
+		if (PointerIn != null)
+			PointerIn(this, e);
+	}
+
+	public virtual void OnPointerOut(LaserEventArgs e)
+	{
+		if (PointerOut != null)
+			PointerOut(this, e);
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+		if (!isActive)
+		{
+			isActive = true;
+			this.transform.GetChild(0).gameObject.SetActive(true);
+		}
+
+		float dist = 100f;
+
+		SteamVR_TrackedController controller = GetComponent<SteamVR_TrackedController>();
+
+		Ray raycast = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		bool bHit = Physics.Raycast(raycast, out hit);
+
+		if (previousContact && previousContact != hit.transform)
+		{
+			LaserEventArgs args = new LaserEventArgs();
+			if (controller != null)
+			{
+				args.controllerIndex = controller.controllerIndex;
+			}
+			args.distance = 0f;
+			args.flags = 0;
+			args.target = previousContact;
+			OnPointerOut(args);
+			previousContact = null;
+		}
+		if (bHit && previousContact != hit.transform)
+		{
+			LaserEventArgs argsIn = new LaserEventArgs();
+			if (controller != null)
+			{
+				argsIn.controllerIndex = controller.controllerIndex;
+			}
+			argsIn.distance = hit.distance;
+			argsIn.flags = 0;
+			argsIn.target = hit.transform;
+			OnPointerIn(argsIn);
+			previousContact = hit.transform;
+		}
+		if (!bHit)
+		{
+			previousContact = null;
+		}
+		if (bHit && hit.distance < 100f)
+		{
+			dist = hit.distance;
+		}
+
+		if (controller != null && controller.triggerPressed)
+		{
+			pointer.transform.localScale = new Vector3(thickness * 5f, thickness * 5f, dist);
+		}
+		else
+		{
+			pointer.transform.localScale = new Vector3(thickness, thickness, dist);
+		}
+		pointer.transform.localPosition = new Vector3(0f, 0f, dist / 2f);
+	}
+}
